dfcb3dc5164e4fe9071ce8421c9fcd7e16c2eca0
gnemek20 <gnemek20@gmail.com>
2024-10-02 18:55:07 +0900

    타이틀, 설명, 대표 이미지 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Kwon Window</title>
+        <meta property='og:description' content='목표는 윈도우처럼' />
+        <meta property='og:image' content='https://llpb2hr4wxbuf0dj.public.blob.vercel-storage.com/peroro-UXjbduNoBA816QJhoiZVDsjKY2RqCx.jpg' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
