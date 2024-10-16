6ae5f48f9e6f2b301771c7305479f9ae46e63405
gnemek20 <gnemek20@gmail.com>
2024-10-17 07:06:02 +0900

    웹의 overflow를 auto로 변경, 프로필의 디자인 추가

--- customWebs/profile.tsx
+++ customWebs/profile.tsx
@@ -1,11 +1,25 @@
 import { Web } from '@/components';
 import { customWebProps } from '@/types/webTypes';
 import styles from '@/styles/profile/profile.module.css';
+import { imageType } from '@/types/publicTypes';
+import Image from 'next/image';
+
+const profileIcon: imageType = {
+  src: require('@/public/images/peroro.jpg'),
+  alt: 'profile'
+}
 
 const Profile = (props: customWebProps) => {
   return props.rendering && (
     <Web name={props.name} rendering={props.rendering} selected={props.selected} selectWeb={() => props.selectWeb()} closeWeb={() => props.closeWeb()}>
-      <p>profile</p>
+      <div className={`${styles.profile}`}>
+        <div className={`${styles.profileIcon}`}>
+          <Image src={profileIcon.src} alt={profileIcon.alt} />
+        </div>
+        <div className={`${styles.search}`}>
+          <input type="text" placeholder="검색어를 입력해주세요" spellCheck={false} />
+        </div>
+      </div>
     </Web>
   );
 }
