17a8f3e34f1651447ff22d46515bdb551d608d1c
gnemek20 <gnemek20@gmail.com>
2024-10-17 20:21:28 +0900

    프로필 리스트 추가

--- customWebs/profile.tsx
+++ customWebs/profile.tsx
@@ -1,6 +1,7 @@
 import { Web } from '@/components';
 import { customWebProps } from '@/types/webTypes';
 import styles from '@/styles/profile/profile.module.css';
+import informationStyles from '@/styles/profile/information.module.css';
 import { imageType } from '@/types/publicTypes';
 import Image from 'next/image';
 
@@ -10,6 +11,29 @@ const profileIcon: imageType = {
 }
 
 const Profile = (props: customWebProps) => {
+  const Information = ({ category, children }: { category: string, children: React.ReactNode }) => {
+    return (
+      <div className={`${informationStyles.informationContainer}`}>
+        <div className={`${informationStyles.information}`}>
+          <p className={`${informationStyles.category}`}>{ category }</p>
+          <div className={`${informationStyles.line}`} />
+          <div className={`${informationStyles.informationList}`}>
+            { children }
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  const InformationBox = ({ name, value }: { name?: string, value: string }) => {
+    return (
+      <div className={`${informationStyles.informationBox}`}>
+        { value && <p className={`${informationStyles.value}`}>{ value }</p> }
+        { name && <p className={`${informationStyles.name}`}>{ name }</p> }
+      </div>
+    );
+  }
+
   return props.rendering && (
     <Web name={props.name} rendering={props.rendering} selected={props.selected} selectWeb={() => props.selectWeb()} closeWeb={() => props.closeWeb()}>
       <div className={`${styles.profile}`}>
@@ -18,6 +42,15 @@ const Profile = (props: customWebProps) => {
         </div>
         <div className={`${styles.search}`}>
           <input type="text" placeholder="검색어를 입력해주세요" spellCheck={false} />
+        </div>
+        <div className={`${styles.information}`}>
+          <Information category="기본 정보">
+            <InformationBox name="이름" value="권민석" />
+            <InformationBox name="나이" value="22" />
+          </Information>
+          <Information category="기타">
+            <InformationBox value="소개 영상" />
+          </Information>
         </div>
       </div>
     </Web>
